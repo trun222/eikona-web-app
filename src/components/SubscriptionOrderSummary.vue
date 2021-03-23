cf411310f92a82bfa5cb91f0c08470235248190e
Thomas R. Underwood II <thomasunderwoodii@gmail.com>
2021-03-23 10:09:32 -0400

    finished account creation template

--- src/components/SubscriptionOrderSummary.vue
+++ src/components/SubscriptionOrderSummary.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="subscription-order-summary">
     <v-container class="mt-5">
-      <v-row class="my-4" justify="start">
+      <v-row class="my-4" justify="center">
         <h1>Subscription Checkout</h1>
       </v-row>
-              
-      <h2>Account Creation</h2>
-      <v-row class="my-2" justify="start">
-        <v-col cols="3">
+
+      <v-row justify="center">
+        <h2>Account Creation</h2>
+      </v-row>
+        
+      <v-row class="my-2" justify="center">
+        <v-col cols="3" md="6">
           <v-sheet color="rgba(209, 191, 183, 1)" elevation="1">
             <!-- Form For Account Creation -->
             <v-form class="pa-4">
               <h2 class="mb-4">Login Details</h2>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Username" prepend-icon="mdi-account-box" solo></v-text-field>
+                  <v-text-field label="Username" prepend-inner-icon="mdi-account-box" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Email" prepend-icon="mdi-at" solo></v-text-field>
+                  <v-text-field label="Email" prepend-inner-icon="mdi-at" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Password" prepend-icon="mdi-lock" solo></v-text-field>
+                  <v-text-field label="Password" prepend-inner-icon="mdi-lock" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Confirm Password" prepend-icon="mdi-lock" solo></v-text-field>
+                  <v-text-field label="Confirm Password" prepend-inner-icon="mdi-lock" solo></v-text-field>
                 </v-col>
               </v-row>
 
@@ -38,7 +41,7 @@
               <h2 class="my-4">User Info</h2>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Full Name" prepend-icon="mdi-account" solo></v-text-field>
+                  <v-text-field label="Full Name" prepend-inner-icon="mdi-account" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
@@ -55,7 +58,7 @@
                       <v-text-field
                         v-model="date"
                         label="Date of Birth"
-                        prepend-icon="mdi-calendar"
+                        prepend-inner-icon="mdi-calendar"
                         readonly
                         v-bind="attrs"
                         v-on="on"
@@ -74,21 +77,26 @@
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="Street Address" prepend-icon="mdi-map-marker" solo></v-text-field>
+                  <v-text-field label="Street Address" prepend-inner-icon="mdi-map-marker" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="City" prepend-icon="mdi-city" solo></v-text-field>
+                  <v-text-field label="City" prepend-inner-icon="mdi-city" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="11">
-                  <v-text-field label="State" prepend-icon="mdi-city" solo></v-text-field>
+                  <v-text-field label="State" solo></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="6">
+                  <v-text-field label="Zipcode" solo></v-text-field>
                 </v-col>
               </v-row>
 
-              <v-btn block>Create</v-btn>
+              <v-btn class="white--text" color="blue" block>Create Account</v-btn>
             </v-form>
           </v-sheet>
         </v-col>
