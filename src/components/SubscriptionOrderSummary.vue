cb363555b31cf12218a8cfdec5043c6b46c7151c
Thomas R. Underwood II <thomasunderwoodii@gmail.com>
2021-03-23 10:27:34 -0400

    finished prototype of SubscriptionsOrderSummary

--- src/components/SubscriptionOrderSummary.vue
+++ src/components/SubscriptionOrderSummary.vue
@@ -4,101 +4,100 @@
       <v-row class="my-4" justify="center">
         <h1>Subscription Checkout</h1>
       </v-row>
-
-      <v-row justify="center">
-        <h2>Account Creation</h2>
-      </v-row>
         
       <v-row class="my-2" justify="center">
         <v-col cols="3" md="6">
-          <v-sheet color="rgba(209, 191, 183, 1)" elevation="1">
+          <v-sheet color="rgba(209, 191, 183, 1)" elevation="3">
             <!-- Form For Account Creation -->
             <v-form class="pa-4">
-              <h2 class="mb-4">Login Details</h2>
+              <h2 class="mb-4">Account Creation</h2>
               <v-row>
-                <v-col cols="11">
+                <v-col>
                   <v-text-field label="Username" prepend-inner-icon="mdi-account-box" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="11">
+                <v-col>
                   <v-text-field label="Email" prepend-inner-icon="mdi-at" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="11">
+                <v-col>
                   <v-text-field label="Password" prepend-inner-icon="mdi-lock" solo></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="11">
+                <v-col>
                   <v-text-field label="Confirm Password" prepend-inner-icon="mdi-lock" solo></v-text-field>
                 </v-col>
               </v-row>
-
-              <v-divider></v-divider>
-
-              <h2 class="my-4">User Info</h2>
-              <v-row>
-                <v-col cols="11">
-                  <v-text-field label="Full Name" prepend-inner-icon="mdi-account" solo></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="11">
-                  <v-menu
-                    ref="menu"
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="auto"
-                  >
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-text-field
-                        v-model="date"
-                        label="Date of Birth"
-                        prepend-inner-icon="mdi-calendar"
-                        readonly
-                        v-bind="attrs"
-                        v-on="on"
-                        solo
-                      ></v-text-field>
-                    </template>
-                    <v-date-picker
-                      ref="picker"
-                      v-model="date"
-                      :max="new Date().toISOString().substr(0, 11)"
-                      min="1950-01-01"
-                      @change="save"
-                    ></v-date-picker>
-                  </v-menu>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="11">
-                  <v-text-field label="Street Address" prepend-inner-icon="mdi-map-marker" solo></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="11">
-                  <v-text-field label="City" prepend-inner-icon="mdi-city" solo></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="11">
-                  <v-text-field label="State" solo></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="6">
-                  <v-text-field label="Zipcode" solo></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-btn class="white--text" color="blue" block>Create Account</v-btn>
             </v-form>
           </v-sheet>
+
+          <v-row class="mt-4">
+            <v-col>
+              <v-btn class="white--text" color="blue" block>Create Account</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+        
+      <v-row class="my-2" justify="center">
+        <v-col cols="3" md="6">
+          <v-sheet color="rgba(209, 191, 183, 1)" elevation="3">
+            <!-- Form For Account Creation -->
+            <v-form class="pa-4">
+              <h2 class="mb-4">Order Summary</h2>
+              <v-row>
+                <v-col>
+                  <v-card
+                      tile
+                    >
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <h4>Subscription</h4>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+
+                      <v-list-item three-line>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <h5>Basic Tier</h5>
+                          </v-list-item-title>
+                          <v-list-item-subtitle>$3.99/mo Annual (Save $12) - 12 months</v-list-item-subtitle>
+
+                          <v-list-item-subtitle>
+                            100 GB of Eikona Cloud Storage
+                          </v-list-item-subtitle>
+
+                          <v-list-item-subtitle>
+                            Subtotal: <b>$47.88</b>
+                          </v-list-item-subtitle>
+
+                          <v-list-item-subtitle>
+                            Tax: <b>$3.35</b>
+                          </v-list-item-subtitle>
+
+                          <v-divider class="my-2"></v-divider>
+
+                          <v-list-item-subtitle>
+                            Total: <b>$51.23</b>
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-card>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-sheet>
+
+          <v-row class="mt-4">
+            <v-col>
+              <v-btn class="white--text" color="green" block>Pay</v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
